919ee03fc43245baa7a3371f23f4a4004be9d3a8
lukepawlicki <98533140+lukepawlicki@users.noreply.github.com>
2022-04-06 03:17:43 +0000

    vowels method

--- StringUtility.py
+++ StringUtility.py
@@ -1,6 +1,27 @@
 class StringUtility:
   def __init__(self, string):
-    j=5
+    self.string = string
 
   def __str__(self):
-    
+    return self.string
+
+  def vowels(self):
+    count = 0
+    for i in self.string:
+      if i == 'a'or i =='e'or i =='i'or i =='o'or i =='u':
+        count += 1
+      if count >= 5: 
+        return 'many'
+    return str(count)
+        
+
+  def bothEnds(self):
+    return (self.string[0,1,-2,-1] if len(self.string[0,1,-2,-1])>2 else '')
+  
+  # def fixStart(self):
+  #   l 
+  
+  # def asciiSum(self):
+  #   l 
+  
+  # def cipher(self):

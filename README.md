a280d7a015679a03215cb0d16a0b09328f8d3bfa
lukepawlicki <98533140+lukepawlicki@users.noreply.github.com>
2022-04-12 00:50:14 +0000

    final

--- README.md
+++ README.md
@@ -8,18 +8,18 @@
 Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ This program is a string library that performs various functions on strings. 
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 0
 
-Grace days remaining: < # >/5
+Grace days remaining: 5/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+ None
 
 ## REFERENCES:
- < List any outside resources used >
+ Python Docs, Stack Overflow
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+ One method was done in one line. 
--- StringUtility.py
+++ StringUtility.py
@@ -1,11 +1,27 @@
 class StringUtility:
   def __init__(self, string):
+    '''
+    Sets the string parameter for the class
+    Args: self- str, object created by the class
+          string- str, word plugged into the class
+    Return: None
+    '''
     self.string = string
 
   def __str__(self):
+    '''
+    Returns the string
+    Args: self- str, instance of the class object
+    Return: self.string- str, returns the object
+    '''
     return self.string
 
   def vowels(self):
+    '''
+    Counts the number of vowels in the string
+    Args: self- str, the word being plugged in
+    Return: count- str, number of vowels in the word
+    '''
     count = 0
     for i in self.string:
       if i == 'a'or i =='e'or i =='i'or i =='o'or i =='u':
@@ -16,12 +32,60 @@ class StringUtility:
         
 
   def bothEnds(self):
+    '''
+    Returns the first and last two characters in the string
+    Args: self- str, word being plugged in
+    Return: modified version of the string, or an empty string
+    '''
     return (self.string[0]+self.string[1]+self.string[-2]+self.string[-1] if len(self.string) >2 else '')
   
-  # def fixStart(self):
-  #   l 
+  def fixStart(self):
+    '''
+    Changes all instances of the first letter to a '*,' except for the first letter itself
+    Args: self- word being plugged in
+    Return: newNewString- str, new string with the above changes
+    '''
+    if len(self.string) <= 1:
+      return self.string
+    else:
+      newString = self.string.replace(self.string[0],'*')
+      newNewString = self.string[0] + newString[1:]
+    return newNewString
+          
+  def asciiSum(self):
+    '''
+    Adds the ord values for all letters in the string
+    Args: self- word being plugged in
+    Return: sum- int, sum of the ords of the letters in the string
+    '''
+    sum = 0
+    for i in range(len(self.string)):
+      sum += ord(self.string[i])
+    return sum
+    
+  def cipher(self):
+    '''
+    Changes all letters by the length of the string, down the alphabet
+    Args: self- word being plugged in
+    Return: cipher- str, new string with incremented letters
+    '''
+    cipher = ""
+    for i in self.string:
+      if i.isalpha():
+        if i.isupper():
+            ordVal = ord(i) + len(self.string)
+            if ordVal > ord('Z'):
+              ordVal = ordVal - 26
+            letter = chr(ordVal)
+        if i.islower():
+            ordVal = ord(i) + len(self.string)
+            if ordVal > ord('z'):
+              ordVal = ordVal - 26
+              if ordVal > ord('z'):
+                ordVal = ordVal - 26
+            letter = chr(ordVal)
+      else:
+        letter = i
+      cipher += letter
+    return cipher
   
-  # def asciiSum(self):
-  #   l 
-  
-  # def cipher(self):
